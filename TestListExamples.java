fb9fc9269e190e6c73cfd1299ec544ff364dc003
aqtlol <matthewzabaco@gmail.com>
2022-11-15 00:46:40 -0800

    commit lab changes

--- TestListExamples.java
+++ TestListExamples.java
@@ -1,6 +1,30 @@
 import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.*;
 
 public class TestListExamples {
   // Write your grading tests here!
+  @Test
+  public void testFilter() {
+      List<String> list = new ArrayList<>();
+      list.add("apple");
+      list.add("mango");
+      list.add("fruit");
+      list.add("strawberry");
+      list.add("pineapple");
+      list.add("grapes");
+
+      StringChecker sc = new ListExamples();
+
+      List<String> expected = new ArrayList<>(); // new String[] {"strawberry", "pineapple"}
+      expected.add("strawberry");
+      expected.add("pineapple");
+
+      assertEquals(expected, ListExamples.filter(list, sc));
+  }
+
+    
 }
--- grade.sh
+++ grade.sh
@@ -1,7 +1,40 @@
 # Create your grading script here
 
-set -e
+#set -e
+
+JU=".:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar"
 
 rm -rf student-submission
 git clone $1 student-submission
+
+echo "Successful clone"
+
+FILE=student-submission/ListExamples.java
+if  [ -f "$FILE" ]; then
+    echo "file exists"
+else
+    echo "file doesnt exist"
+    exit 1
+fi
+
+cp -r lib student-submission
+echo "Junit copied"
+
+cp TestListExamples.java student-submission/
+cd student-submission
+echo "In student-submission"
+
+javac -cp $JU *.java 2> compileError.txt
+cat compileError.txt
+set -e
+echo "Compiled"
+
+java -cp $JU org.junit.runner.JUnitCore TestListExamples
+
+echo "Ran da tests"
+
+
+
+
+
 
